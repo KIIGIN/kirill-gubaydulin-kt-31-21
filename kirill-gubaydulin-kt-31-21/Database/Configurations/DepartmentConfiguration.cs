2951a7bab824262474cbfe6e4ca873f4c5c34b4d
Kirill G. <gubaydulin2004@gmail.com>
2024-10-18 15:26:54 +0300

    Added creation date and department heads to models and migrations

--- kirill-gubaydulin-kt-31-21/Database/Configurations/DepartmentConfiguration.cs
+++ kirill-gubaydulin-kt-31-21/Database/Configurations/DepartmentConfiguration.cs
@@ -26,6 +26,20 @@ namespace kirill_gubaydulin_kt_31_21.Database.Configurations
                 .HasColumnType(ColumnType.String)
                 .HasMaxLength(100);
 
+            // Teacher
+            builder.HasOne(p => p.Teacher)
+                .WithMany()
+                .HasForeignKey(p => p.TeacherId)
+                .HasConstraintName("fk_teacher_id")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasIndex(
+                p => p.TeacherId, $"idx_{TableName}_fk_teacher_id"
+            );
+
+            builder.Navigation(p => p.Teacher)
+                .AutoInclude();
+
             builder.ToTable(TableName);
         }
     }
